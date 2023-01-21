040b06cb02ddf4a7c56152821306700cef805e18
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-01-20 21:29:06 -0300

    Set logo and phosphor icons

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,28 @@
 import './styles/global.css';
+import { Plus } from 'phosphor-react';
 import { Habit } from './components/Habit'
 
-function App() {
+import logoImage from './assets/logo.svg'
 
-
+export function App() {
   return (
-    <div>
-    <h1>Hello World</h1>
-    <Habit completed={10}/>
-    <Habit completed={18}/>
-    <Habit completed={37}/>
+
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <div className=" w-full max-w-3xl mx-auto flex items-center justify-between">
+        <img src={logoImage} alt="Habits" />
+
+        <button type="button" className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
+          <Plus size={20} className="text-violet-500"/>
+          Novo Hábito
+        </button>
+        </div>
+
+      </div>
+
     </div>
 
   )
 }
 
-export default App
+
